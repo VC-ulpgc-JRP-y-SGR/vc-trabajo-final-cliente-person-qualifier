3b8e35db9657de45436c37fbaa595ba6b13d491c
josericardopenase <josericardopenase@gmail.com>
2024-01-16 16:31:37 +0000

    Finished cam client

--- main.py
+++ main.py
@@ -1,25 +1,69 @@
 import cv2
 import logging
+import queue
+import threading
+import argparse
 from face_detector.mediapipe import MediaPipeFaceDetector
-from face_detector.viola_jones import ViolaJonesFaceDetector
 from server import CameraServer
 
+# Initialize argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", help="IP address of the server", default="127.0.0.1")
+parser.add_argument("--port", help="Port of the camera", default="500")
+args = parser.parse_args()
+
+# Initialize camera and server
 cap = cv2.VideoCapture(0)
-server = CameraServer(ip="127.0.0.1", buffer_size= 4096, port=5005)
+server = CameraServer(ip=args.ip, buffer_size=4096, port=args.port)
 server.start()
 
-while True:
-    # Read frame from camera
-    # Send frame
-    try:
-        frame = cap.read()[1]
-        face_detector = MediaPipeFaceDetector()
-        for face in face_detector.detect(frame):
-            cv2.rectangle(frame, (face.bounding_box.origin.x, face.bounding_box.origin.y), (face.bounding_box.end.x, face.bounding_box.end.y), (255, 0, 0), 2)
-        server.send_frame(frame)
-    except Exception as e:
-        logging.error(f"Failed to send frame: {e}")
-        break
+# Initialize queue and flag for thread communication
+frame_queue = queue.Queue()
+stop_flag = threading.Event()
 
-# Close connection
-server.close()
+# Function for the sending thread
+def send_frames():
+    while not stop_flag.is_set():
+        while not frame_queue.empty():
+            # Check if the queue has more than 10 frames
+            if frame_queue.qsize() > 10:
+                # Discard the oldest frame
+                frame_queue.get_nowait()
+            else:
+                break
+
+        if not frame_queue.empty():
+            frame = frame_queue.get()
+            try:
+                server.send_frame(frame)
+            except Exception as e:
+                logging.error(f"Failed to send frame: {e}")
+                stop_flag.set()
+
+
+# Start the sending thread
+send_thread = threading.Thread(target=send_frames)
+send_thread.start()
+
+# Main loop
+try:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2.imshow('frame', frame)
+        frame_queue.put(frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+except Exception as e:
+    logging.error(f"Error in main loop: {e}")
+finally:
+    # Signal the sending thread to stop and wait for it
+    stop_flag.set()
+    send_thread.join()
+
+    # Release resources
+    cap.release()
+    cv2.destroyAllWindows()
+    server.close()
